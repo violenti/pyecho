eab8d3fd8eb986698569847bd5c70a2f28626533
David Fernandez <rodavidfernandez@gmail.com>
2018-11-26 12:56:26 -0300

    fix print stdout for kubernetes

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,9 @@ FROM python:3.7.1
 RUN adduser --disabled-password --gecos '' app
 WORKDIR /home/app
 COPY requirements.txt .
-RUN python -m pip install -r /home/app/requirements.txt
+RUN python -m pip install -r requirements.txt
 COPY main.py .
 USER app
+ENV FLASK_ENV="development"
 EXPOSE 8080
-CMD ["python","main.py"]
+CMD ["python","-u","main.py"]
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/local/bin/python3
-
+import os
 from flask import Flask, request
 from flask import jsonify
+import sys
 
 app = Flask(__name__)
 
@@ -10,16 +11,19 @@ def echo ():
 
     return "echo ok\n"
 
-@app.route('/test',methods = ['POST', 'GET'])
+@app.route('/test', methods = ['POST', 'GET'])
 def test():
-   if request.method == 'POST':
-      print (request.is_json)
-      content = request.get_json()
-      print (content)
-      return "POST ok"
-   else:
+    if request.method == 'POST':
+       print (request.is_json)
+       content = request.get_json()
+       print (content,file=sys.stderr)
+     
 
-      return "GET ok"
+       return "POST ok"
+    else:
+       return "GET ok"
+
+
 if __name__ == '__main__':
 
-  app.run(host='0.0.0.0', port=8080,debug=True)
+    app.run(debug=os.environ['FLASK_ENV'] == 'development',host='0.0.0.0', port=8080)
